9d937d740c838017cde497a03133085fae342ca0
juliayesterday <164638074+juliayesterday@users.noreply.github.com>
2024-03-23 17:00:32 +0200

    4 commit

--- js/spiner.js
+++ js/spiner.js
@@ -2,8 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const spinnerOverlay = document.getElementById("spinner-overlay");
 
     spinnerOverlay.style.display = "flex";
-
-
     window.addEventListener("load", function() {
         spinnerOverlay.style.display = "none";
     });
